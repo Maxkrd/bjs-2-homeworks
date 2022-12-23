2864214cf1ccdd5b84f3e574e2f21303aea29aea
Maxkrd <mymailkrd@yandex.ru>
2022-12-23 13:13:31 +0300

    Решение первого задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,35 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2)-4*a*c;
   
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  } else if (d === 0) {
+      arr[0] = -b/(2*a);
+  } else {
+  
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P;
+  let loanBody;
+  let monthlyFee;
+  if (isNaN(percent)) {
+    return false;
+  } else if (isNaN(contribution)) {
+    return false;
+  } else if (isNaN(amount)) {
+    return false;
+  } else if (isNaN(countMonths)) {
+    return false;
+  } else {
+    P = percent / 100 / 12;
+    loanBody = amount - contribution;
+    monthlyFee = loanBody * (P + (P / ((Math.pow((1 + P), countMonths)) - 1)));
+    return Number((monthlyFee * countMonths).toFixed(2));
+  }
 }
