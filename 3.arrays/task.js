b76fa2c29a013915a68f005d02a9b0d0b7c54266
Maxkrd <mymailkrd@yandex.ru>
2023-01-04 09:15:30 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
-}
+  let compare = arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i]);
+  return compare; 
+  }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let result = (users.filter(user => user.gender === gender));
+  if (result.length > 0) {
+    result = result.reduce((r, i) => r + i.age, 0) / (result.length);
+    return result;
+  } else {
+    return 0;
+  }
 }
